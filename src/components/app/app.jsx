f663481d23d4743d0a4dc021da0702c9ce6d359c
Evgeny Karpel <kspshnik@yandex.ru>
2021-12-14 01:24:26 +0300

    [New] В App добавлена обёртка <main>, сделано выравнивание дочерних компонент по сетке.

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import AppHeader from '../app-header/app-header';
 
 import '@ya.praktikum/react-developer-burger-ui-components';
-// import appStyles from 'app.module.css';
+import appStyles from './app.module.css';
 
 const App = () => {
   return (
     <>
       <AppHeader />
+      <main className= {appStyles.main}>
+      
+      </main>
     </>
   )
 }
