14a830285ec6e9a31e620d4242a1ffb0912b7329
Evgeny Karpel <kspshnik@yandex.ru>
2021-12-13 22:00:39 +0300

    [New]: App.jsx

---
+++ src/components/app/app.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import AppHeader from '../app-header/app-header';
+
+import '@ya.praktikum/react-developer-burger-ui-components';
+import appStyles from 'app.module.css';
+
+const App = () => {
+  return (
+   // <AppHeader />
+  )
+}
