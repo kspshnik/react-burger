e7b11ce735c6eda7dacbce5c3988c8c0dfb2488d
Evgeny Karpel <kspshnik@yandex.ru>
2021-12-13 22:07:46 +0300

    [Fix]: App.jsx - добавлен забытый React.Fragment и закомментированы пока неактуальные импорты.

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import AppHeader from '../app-header/app-header';
+// import AppHeader from '../app-header/app-header';
 
 import '@ya.praktikum/react-developer-burger-ui-components';
-import appStyles from 'app.module.css';
+// import appStyles from 'app.module.css';
 
 const App = () => {
   return (
-   // <AppHeader />
+    <>
+    </>
   )
 }
+
+export default App;
