5edafa40b2bbbfc512d21dea6337bcfbcc395e00
Evgeny Karpel <kspshnik@yandex.ru>
2021-12-14 01:14:30 +0300

    [Feature] Сверстана и работает шапка сайта: компонент AppHeader.

---
+++ src/components/app-header/app-header.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+import { Logo, BurgerIcon, ListIcon, ProfileIcon } from "@ya.praktikum/react-developer-burger-ui-components";
+
+import headerStyles from './app-header.module.css';
+
+const AppHeader = () => {
+  
+  return (
+    <header className={`${headerStyles.header} mt-10 mr-10 ml-10`} >
+      <ul className={`${headerStyles.content}`}>
+        <li className={headerStyles.menu__item}>
+          <nav className={headerStyles.menu__wrapper}>
+            <ul className={headerStyles.menu}>
+              <li className={`${headerStyles.menu__item}  pl-5 pr-5`}>
+                <BurgerIcon type="primary" />
+                <p className="text text_type_main-default ml-2">Конструктор</p>
+              </li>
+              <li className={`${headerStyles.menu__item} pl-5 pr-5`}>
+                <ListIcon type="secondary" />
+                <p className="text text_type_main-default text_color_inactive ml-2">Лента заказов</p>
+              </li>
+            </ul>
+          </nav>
+        </li>
+        <li>
+          <Logo />
+        </li>
+        <li className={headerStyles.menu__profile}>
+          <ProfileIcon type="secondary" />
+          <p className="text text_type_main-default text_color_inactive ml-2">Личный кабинет</p>
+        </li>
+      </ul>
+    </header>
+  )
+}
+
+export default AppHeader;
