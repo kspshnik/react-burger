3358bd1d8aeefa4c05d757e2a018168610a9b662
Evgeny Karpel <kspshnik@yandex.ru>
2022-01-30 15:58:17 +0300

    [Cleanup] Удален ненужный файл с хардкодом заказа

--- src/utils/data.js
+++
@@ -1,9 +0,0 @@
-const initialOrder = ['60d3b41abdacab0026a733c6',
-  '60d3b41abdacab0026a733c9',
-  '60d3b41abdacab0026a733ce',
-  '60d3b41abdacab0026a733d0',
-  '60d3b41abdacab0026a733d0',
-  '60d3b41abdacab0026a733d1',
-];
-
-export default initialOrder;
