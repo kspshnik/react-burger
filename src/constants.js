95d98667eb3e7130908a0e6217b679029bd27b29
Evgeny Karpel <kspshnik@yandex.ru>
2022-02-01 01:42:29 +0300

    [New] Вынесение констант.

--- src/constants.js
+++ src/constants.js
@@ -12,27 +12,13 @@ const SAUCE = 'sauce';
 const MAIN = 'main';
 const INGREDIENT = 'ingredient';
 const ORDER = 'order';
-const ERROR = 'error';
-const CLOSE = 'close';
 
-const ACTION_CLOSE = { type: CLOSE };
-const ACTION_OPEN_INGREDIENT = { type: INGREDIENT };
-const ACTION_OPEN_ORDER = { type: ORDER };
-const ACTION_SHOW_ERROR = { type: ERROR };
-
-const BAD_ORDER_NO = '000000';
 export {
   BUN,
   SAUCE,
   MAIN,
   INGREDIENT,
   ORDER,
-  ERROR,
-  CLOSE,
-  ACTION_CLOSE,
-  ACTION_OPEN_INGREDIENT,
-  ACTION_OPEN_ORDER,
-  ACTION_SHOW_ERROR,
   BACKEND_ROUTES,
-  BAD_ORDER_NO,
+
 };
