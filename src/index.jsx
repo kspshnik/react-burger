433d5437efe6038af81f6ca9bc1422f4d4fe715c
Evgeny Karpel <kspshnik@yandex.ru>
2022-02-01 01:40:38 +0300

    [Setup] Включена трассировка в Redux DevTools

--- src/index.jsx
+++ src/index.jsx
@@ -35,7 +35,7 @@ Sentry.init({
 const sentryReduxEnhancer = Sentry.createReduxEnhancer({});
 
 const composeEnhancers = typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ trace: true, traceLimit: 25 })
   : compose;
 
 const enhancer = composeEnhancers(applyMiddleware(thunk), sentryReduxEnhancer);
