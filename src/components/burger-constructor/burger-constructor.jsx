d84b8021775abff5d92eb03c410cdf9a30b18232
Evgeny Karpel <kspshnik@yandex.ru>
2022-02-01 13:44:46 +0300

    [Hotfix + Refactor] DropZone + объявление на пустом экране

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -1,41 +1,32 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 import { Button, ConstructorElement, CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 
 import { useSelector, useDispatch } from 'react-redux';
 
-import { useDrop } from 'react-dnd';
 import bcStyles from './burger-constructor.module.css';
 import ScrollArea from '../scroll-area/scroll-area';
 import ConstructorGrid from '../constructor-grid/constructor-grid';
 import placeOrder from '../../services/thunks/place-order';
 import { insertInterior, setBun } from '../../services/actionCreators';
 import { BUN, INGREDIENT } from '../../constants';
+import DropZone from '../drop-zone/drop-zone';
 
 const BurgerConstructor = () => {
   const { all } = useSelector((store) => store.ingredients);
   const { bun, choice } = useSelector((state) => state.orders);
   const dispatch = useDispatch();
 
-  // eslint-disable-next-line no-use-before-define
-  const onDrop = () => handleDrop();
+  const isEmpty = () => !(!!bun || choice.length > 0);
 
-  const [{ isOver, dropItem }, dropRef] = useDrop({
-    accept: INGREDIENT,
-    collect: (monitor) => ({
-      isOver: monitor.isOver(),
-      dropItem: monitor.getItem(),
-    }),
-    drop: onDrop,
-  });
-
-  function handleDrop() {
+  const handleDrop = (dropItem) => {
     if (dropItem.type === BUN) {
       dispatch(setBun(dropItem));
     } else {
-      dispatch(insertInterior(dropItem, 0));
+      dispatch(insertInterior(dropItem));
     }
-  }
+  };
+
   const calculateTotal = React.useMemo(() => {
     if (!(!!all && !!bun)) {
       return 0;
@@ -52,39 +43,43 @@ const BurgerConstructor = () => {
     dispatch(placeOrder(totalOrder));
   };
 
-  useEffect(() => {
-    if (all && !bun) {
-      dispatch(setBun(Object.values(all).find((item) => item?.type === 'bun')));
-    }
-  }, [all, bun, dispatch]);
-
   return (
     <section className={`${bcStyles.section} mt-25 mb-10`}>
-      <main ref={dropRef} className={`${bcStyles.wrapper} ${isOver ? bcStyles.wrapper_engaged : ''}`}>
-        {!!bun && (
-        <div className='pb-2'>
-          <ConstructorElement
-            type='top'
-            isLocked
-            text={`${bun?.name} (верх)`}
-            thumbnail={bun?.image_mobile}
-            price={bun.price} />
-        </div>
+      <DropZone
+        handleDrop={handleDrop}
+        type={INGREDIENT}
+        contentClass={bcStyles.drop}
+        hoverClass={bcStyles.drop_engaged}>
+        { isEmpty() ? (
+          <h2 className='pl-5 pr-5 pt-15 text text_type_main-large'>Пожалуйста, выберите булку, начинки и соусы</h2>
+        ) : (
+          <main className={bcStyles.wrapper}>
+            {!!bun && (
+            <div className='pb-2'>
+              <ConstructorElement
+                type='top'
+                isLocked
+                text={`${bun?.name} (верх)`}
+                thumbnail={bun?.image_mobile}
+                price={bun.price} />
+            </div>
+            )}
+            <ScrollArea contentClass={`${bcStyles.scroll} custom-scroll`}>
+              <ConstructorGrid />
+            </ScrollArea>
+            {!!bun && (
+            <div className='pt-2 pb-10'>
+              <ConstructorElement
+                type='bottom'
+                isLocked
+                text={`${bun?.name} (низ)`}
+                thumbnail={bun?.image_mobile}
+                price={bun?.price} />
+            </div>
+            )}
+          </main>
         )}
-        <ScrollArea contentClass={`${bcStyles.scroll} custom-scroll`}>
-          <ConstructorGrid />
-        </ScrollArea>
-        {!!bun && (
-        <div className='pt-2 pb-10'>
-          <ConstructorElement
-            type='bottom'
-            isLocked
-            text={`${bun?.name} (низ)`}
-            thumbnail={bun?.image_mobile}
-            price={bun?.price} />
-        </div>
-        )}
-      </main>
+      </DropZone>
       <footer className={`${bcStyles.footer} mb-3 mr-5`}>
         <div className={`${bcStyles.price} mr-10`}>
           <p className='text text_type_digits-medium pr-2'>
