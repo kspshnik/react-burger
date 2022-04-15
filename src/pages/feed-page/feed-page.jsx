1a6235aca42dbfc9ad9ff7cf50f5cc32ac9ae152
Evgeny Karpel <kspshnik@yandex.ru>
2022-04-15 22:08:38 +0300

    [New] Эффект получения публичного фида.

--- src/pages/feed-page/feed-page.jsx
+++ src/pages/feed-page/feed-page.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import UnderConstruction from '../../components/under-construction/under-construction';
+import { startPublicFeed, stopPublicFeed } from '../../services/actionCreators';
 
-const FeedPage = () => (<UnderConstruction />);
+const FeedPage = () => {
+  const dispatch = useDispatch();
+  React.useEffect(() => {
+    dispatch(startPublicFeed());
+    return () => {
+      dispatch(stopPublicFeed());
+    };
+  }, [dispatch]);
+  return (<UnderConstruction />);
+};
 
 export default FeedPage;
