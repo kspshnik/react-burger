8cbbfea2591df12ed2a5af41eeda6ebaea1856ce
Evgeny Karpel <kspshnik@yandex.ru>
2022-04-15 22:09:06 +0300

    [New] Дополнение хелпера

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,7 +1,8 @@
 import reorderChoice from './reorder-choice';
 import stripBearer from './strip-bearer';
+import prepareFeed from './prepare-feed';
 import { nameValidity, emailValidity, passwordValidity } from './validate';
 
 export {
-  reorderChoice, stripBearer, nameValidity, emailValidity, passwordValidity,
+  reorderChoice, stripBearer, nameValidity, emailValidity, passwordValidity, prepareFeed,
 };
