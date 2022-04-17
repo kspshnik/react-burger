2f3cea371ac61bcb45a6f7e8622ceac70c3a3eab
Evgeny Karpel <kspshnik@yandex.ru>
2022-04-18 00:35:33 +0300

    [New] Лента заказов пользователя

--- src/components/orders-feed/orders-feed.jsx
+++ src/components/orders-feed/orders-feed.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
-import UnderConstruction from '../under-construction/under-construction';
 import { startPrivateFeed, stopPrivateFeed } from '../../services/actionCreators';
+import OrderRibbon from '../order-ribbon/order-ribbon';
+import { PRIVATE } from '../../constants';
+import LoaderProtector from '../loader-protector/loader-protector';
 
 const OrdersFeed = () => {
   const dispatch = useDispatch();
+  const isFeedLoaded = useSelector((state) => !!state?.feed?.private?.orders);
+  const isIngredientsLoaded = useSelector((state) => !!state?.ingredients?.all);
 
   React.useEffect(() => {
     dispatch(startPrivateFeed());
@@ -15,7 +19,12 @@ const OrdersFeed = () => {
     };
   }, [dispatch]);
 
-  return (<UnderConstruction />);
+  return (
+    <LoaderProtector isLoaded={isFeedLoaded && isIngredientsLoaded}>
+      <OrderRibbon feedType={PRIVATE} />
+    </LoaderProtector>
+
+  );
 };
 
 export default OrdersFeed;
