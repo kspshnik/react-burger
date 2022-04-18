8a865630f89648981cbc64c5baaade4118e9c90e
Evgeny Karpel <kspshnik@yandex.ru>
2022-04-18 15:40:41 +0300

    [Refactor] Вынесение хелперов

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,8 +1,17 @@
 import reorderChoice from './reorder-choice';
 import stripBearer from './strip-bearer';
 import prepareDateTime from './prepare-date-time';
+import calculateTotal from './calculate-total';
+import statusName from './status-name';
 import { nameValidity, emailValidity, passwordValidity } from './validate';
 
 export {
-  reorderChoice, stripBearer, nameValidity, emailValidity, passwordValidity, prepareDateTime,
+  reorderChoice,
+  stripBearer,
+  nameValidity,
+  emailValidity,
+  passwordValidity,
+  prepareDateTime,
+  calculateTotal,
+  statusName,
 };
