77202cff8bea46415d68506be0e82d316a25f9cc
Evgeny Karpel <kspshnik@yandex.ru>
2022-05-27 16:28:23 +0300

    [Setup] Поднастроим ESLint под TS

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,9 +5,9 @@ module.exports = {
     node: false,
     jest: true,
     mongo: false,
-    es6: true,
+    es6: true
   },
-  plugins: ['babel', 'prefer-arrow', 'ternary', 'promise', 'import', 'jsx-a11y'],
+  plugins: ['prefer-arrow', 'ternary', 'promise', 'import', 'jsx-a11y', "@typescript-eslint"],
   extends: [
     'plugin:react-hooks/recommended',
     'plugin:ternary/recommended',
@@ -17,39 +17,40 @@ module.exports = {
     'eslint:recommended',
     'plugin:react/recommended',
     'airbnb',
+    'airbnb-typescript',
     'airbnb/hooks',
     'plugin:jsx-a11y/strict',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
   ],
-  "parser": "babel-eslint", // Парсер для обработки jsx кода
+  "parser": "@typescript-eslint/parser",
   "parserOptions": {
     "ecmaFeatures": {
       "jsx": true,
-      impliedStrict: 'true',
+      impliedStrict: 'true'
     },
-    "ecmaVersion": 12, // версия стандарта JavaScript. Последний 12 (2021).
-    "sourceType": "module" // Позволяет использовать import/export
+    "ecmaVersion": 12, // Версия стандарта JavaScript. Последний 12 (2021).
+    "sourceType": "module", // Позволяет использовать import/export
+    project: './tsconfig.json'
   },
-  ignorePatterns: ['*.cjs', 'src/reportWebVitals.js', 'src/reportWebVitals.ts', 'src/index.ts'],
+  ignorePatterns: ['*.cjs', 'src/reportWebVitals.js', 'src/reportWebVitals.ts'],
   rules: {
-    'no-underscore-dangle': [
-      'error',
-      {
-        allow: [
-          '_id',
-          '__REDUX_DEVTOOLS_EXTENSION_COMPOSE__',
-        ],
-        enforceInMethodNames: true,
-        allowAfterThis: true,
-      },
-    ],
+    'no-underscore-dangle': ['error', {
+      allow: ['_id', '__REDUX_DEVTOOLS_EXTENSION_COMPOSE__'],
+      enforceInMethodNames: true,
+      allowAfterThis: true
+    }],
     'jsx-quotes': ['error', 'prefer-single'],
     'react/jsx-closing-bracket-location': ['error', 'after-props'],
     'class-methods-use-this': ['error'],
-    'react/jsx-filename-extension': [1, {extensions: ['.js', '.jsx']}],
+    'react/jsx-filename-extension': [1, {
+      extensions: ['.js', '.jsx', '.ts', '.tsx']
+    }],
     'react/forbid-prop-types': [0],
     "react/function-component-definition": ['error', {
       'namedComponents': 'arrow-function',
-      'unnamedComponents': 'arrow-function',
+      'unnamedComponents': 'arrow-function'
     }],
     'camelcase': ["error", {allow: ["^image_"]}],
     'prefer-arrow/prefer-arrow-functions':
